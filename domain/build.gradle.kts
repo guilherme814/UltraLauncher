03ebb48541a6443730ffaf428f51e8302bca4868
guilherme814 <ksksgggvvhjj@gmail.com>
2026-06-30 19:14:08 -0300

    Add domain module build.gradle.kts

---
+++ domain/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    kotlin("jvm")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+}
